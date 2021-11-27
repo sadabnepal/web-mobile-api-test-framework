dd8b1753ddd02c3fb5e8280a47729993cdfce208
rohit <sadabnepal1993@gmail.com>
2021-11-27 22:39:25 +0530

    changed to anonymous function

--- src/utils/WaitUtils.ts
+++ src/utils/WaitUtils.ts
@@ -1,18 +1,13 @@
 import { ChainablePromiseElement } from 'webdriverio';
 
-class WaitUtils {
-
-    async waitForElementToBeDisplayed(element: ChainablePromiseElement<Promise<WebdriverIO.Element>>, waitTime?: number) {
-        if (waitTime) {
-            await element.waitForDisplayed({ timeout: waitTime });
-        } else {
-            await element.waitForDisplayed();
-        }
+export const waitForElementToBeDisplayed = async (element: ChainablePromiseElement<Promise<WebdriverIO.Element>>, waitTime?: number) => {
+    if (waitTime) {
+        await element.waitForDisplayed({ timeout: waitTime });
+    } else {
+        await element.waitForDisplayed();
     }
-
-    async waitUntilTextLoaded(element: ChainablePromiseElement<Promise<WebdriverIO.Element>>, expectedText: string) {
-        await element.waitUntil(async () => await element.getText() === expectedText, { timeout: 10000, interval: 100 });
-    }
-
 }
-export default new WaitUtils()
+
+export const waitUntilTextLoaded = async (element: ChainablePromiseElement<Promise<WebdriverIO.Element>>, expectedText: string) => {
+    await element.waitUntil(async () => await element.getText() === expectedText, { timeout: 10000, interval: 100 });
+}
