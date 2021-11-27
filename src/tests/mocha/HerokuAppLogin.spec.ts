c75f7fe1ca7bab0e0229f7ea253141cc7f78b4d7
rohit <sadabnepal1993@gmail.com>
2021-11-27 22:33:53 +0530

    added beforeEach hook

--- src/tests/mocha/HerokuAppLogin.spec.ts
+++ src/tests/mocha/HerokuAppLogin.spec.ts
@@ -2,19 +2,19 @@ import LoginPage from '../../pages/login.page';
 import SecurePage from '../../pages/secure.page';
 import LoginConstants from '../../static/loginConstants';
 
-describe('My Login application', () => {
+describe('Test herokuapp application login', () => {
+
+    beforeEach(async () => {
+        await LoginPage.openApp();
+    })
 
     it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(LoginConstants.LOGIN_SUCCESS_MSG);
     });
 
     it('should not login with invalid credentials', async () => {
-        await LoginPage.open();
-
         await LoginPage.login('foo', 'baar');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(LoginConstants.LOGIN_FAILED_MSG);
