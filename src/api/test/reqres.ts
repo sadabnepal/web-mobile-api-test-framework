27be6fcc0e6129747b5dc9b169832763c53fbb01
rohit <sadabnepal1993@gmail.com>
2021-12-02 13:12:33 +0530

    updated scenario names

--- src/api/test/reqres.ts
+++ src/api/test/reqres.ts
@@ -6,21 +6,21 @@ import { stringFormatter } from 'src/api/utils/formatter';
 
 describe('REQRES users api validation', () => {
 
-    it('should call POST user', async () => {
+    it('should verify POST user call', async () => {
         const response = await makePOSTCall(endpoints.USERS_SERVICE, createUserPayload)
         assert.equal(response.statusCode, 201)
         assert.equal(response.body.name, createUserPayload.name)
         assert.equal(response.body.job, createUserPayload.job)
     });
 
-    it('should call GET user/{id}', async () => {
+    it('should verify GET user/{id} call', async () => {
         const userByID = stringFormatter(endpoints.USER_BY_ID_SERVICE, 2)
         const response = await makeGETCall(userByID)
         assert.equal(response.statusCode, 200)
         assert.equal(response.body.data.id, 2)
     });
 
-    it('should call DELETE user/{id}', async () => {
+    it('should verify DELETE user/{id} call', async () => {
         const userByID = stringFormatter(endpoints.USER_BY_ID_SERVICE, 2)
         const response = await makeDELETECall(userByID)
         assert.equal(response.statusCode, 204)
