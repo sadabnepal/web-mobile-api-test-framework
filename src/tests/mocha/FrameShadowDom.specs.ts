0e55978f6769fd06095c39d046ae6b8747d11b13
rohit <sadabnepal1993@gmail.com>
2021-12-19 22:30:36 +0530

    added assertion for shadow dom tests

--- src/tests/mocha/FrameShadowDom.specs.ts
+++ src/tests/mocha/FrameShadowDom.specs.ts
@@ -1,22 +1,24 @@
 import frameShadowPage from "src/pages/frameShadowDom.page"
+import testdata from "src/resources/testdata.json"
 
-describe('Shadow dom and frame demo', () => {
+describe.only('Shadow dom and frame demo', () => {
 
-    beforeEach(async ()=> {
+    beforeEach(async () => {
         await frameShadowPage.openApp()
     })
 
     it('should switch to frame and handle shadow dom element', async () => {
         await browser.switchToFrame(await frameShadowPage.snacksFrame)
-        await frameShadowPage.snacksTextbox.shadow$("#tea").setValue('Lemon Tea')
-        await browser.pause(2000)
+        const shadowTeaElement = frameShadowPage.snacksShadowDom.shadow$("#tea")
+        await shadowTeaElement.setValue(testdata.shadowDomData.tea)
+        await expect(shadowTeaElement).toHaveValue(testdata.shadowDomData.tea)
     })
 
     it('should switch to frame inside shadow dom', async () => {
         const frameElement = await frameShadowPage.username.shadow$("#pact1")
         await browser.switchToFrame(frameElement);
-        await frameShadowPage.country.setValue('Nepal');
-        await browser.pause(2000)
+        await frameShadowPage.country.setValue(testdata.shadowDomData.country);
+        await expect(frameShadowPage.country).toHaveValue(testdata.shadowDomData.country)
     })
-   
+
 })
