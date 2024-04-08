6a8377f2e8909eaff0e482f145f4d355ba890dc4
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2024-04-08 20:54:58 +1000

    fixed test failure of wrong import

--- web/utils/WaitUtils.ts
+++
@@ -1,9 +0,0 @@
-import { WebdriverIOElement } from '@UITypes/webelements';
-
-export const waitForElementToBeDisplayed = async (element: WebdriverIOElement, waitTime?: number) => {
-    await element.waitForDisplayed({ timeout: waitTime ? waitTime : 10000 });
-}
-
-export const waitUntilTextLoaded = async (element: WebdriverIOElement, expectedText: string) => {
-    await element.waitUntil(async () => await element.getText() === expectedText, { timeout: 10000, interval: 100 });
-}
